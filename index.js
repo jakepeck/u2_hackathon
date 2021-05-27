b62b5c8987c1dc164f0caf0823ed3601c87ded70
Lindsey Moss <llmodbls@gmail.com>
2021-05-27 15:36:37 -0400

    initial commit, cleaned baseline, dependencies added

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+// Your Code Here
+
+
+// Your Code Ends Here
+app.listen(PORT, () => {
+  console.log(`App listening on port: ${PORT}`)
+})
