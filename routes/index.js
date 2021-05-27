ba400a10e139a3506771a380a907e9ec7f3df914
Jake Peck <jakepeck44@gmail.com>
2021-05-27 16:42:19 -0500

    connected controllers to routes

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
-const { Router } = require('express');
+const { Router } = require('express')
 const controllers = require('../controllers')
-const router = Router();
+const router = Router()
 
-module.exports = router;
+router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/orders', controllers.createOrder)
+
+router.get('/orders', controllers.getAllOrders)
+
+router.get('/orders/:id', controllers.getOrderById)
+
+router.put('/orders/:id', controllers.updateOrder)
+
+router.delete('/orders/:id', controllers.deleteOrder)
+
+module.exports = router
