c7d7d02a71a1a83155a9abb19d760baf68830ee1
Lindsey Moss <llmodbls@gmail.com>
2021-05-27 16:12:44 -0400

    barebones components, pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
+import React, {Component} from 'react';
 import './styles/App.css';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-     
-     
-    </div>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      chosenPage: null, // Will point to displaying a page once state sets
+    }
+  }
+
+  // functions go here
+
+  render() {
+    return (
+      <div className="App">
+       
+  // visible app stuff goes here
+  // consider conditional rendering
+       
+      </div>
+    );
+
+  }
 }
 
-export default App;
