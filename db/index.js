71f77f98d321b1d689a1de2338155fdb6c364851
Bailey Jean <baileyjleavitt@gmail.com>
2021-05-28 08:56:20 -0700

    Delete unnecessary Order files. Tying to fix POST route for menuitem

--- db/index.js
+++ db/index.js
@@ -3,7 +3,10 @@ const mongoose = require('mongoose')
 let MONGODB_URI = 'mongodb://127.0.0.1:27017/menuDatabase'
 
 mongoose
-    .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .connect(MONGODB_URI, { 
+        useUnifiedTopology: true, 
+        useNewUrlParser: true, 
+        useFindAndModify: true })
     .then(() => {
         console.log('Successfully connected to MongoDB.')
     })
