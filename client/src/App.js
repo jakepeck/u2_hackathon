3f15bd6c6592db833d756818fb5a477fdfc84d6c
Jake Peck <jakepeck44@gmail.com>
2021-05-28 11:10:09 -0500

    blah blah

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
-import './styles/App.css';
-import {Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react'
+import './styles/App.css'
+import { Route, Switch } from 'react-router-dom'
 import axios from 'axios'
 import { BASE_URL } from './globals'
 import Nav from './components/Nav'
@@ -25,8 +25,8 @@ export default class App extends Component {
 
   async componentDidMount() {
     const res = await axios.get(`${BASE_URL}/menuitems`)
-    this.setState({ menu: res.data.results })
-
+    console.log(res.data)
+    this.setState({ menu: res.data.menuitems })
   }
 
   chooseItem = (itemID) => {
@@ -35,70 +35,86 @@ export default class App extends Component {
     })
   }
 
-
   render() {
-
-for (let i=0;i<this.state.menu.length; i++) {
-  switch (this.state.menu[i].type) {
-    case 'app' :
-      this.state.appMenu.push(this.state.menu[i])
-      break;
-    case 'dessert' :
-      this.state.dessertMenu.push(this.state.menu[i])
-      break;
-    case 'dinner' :
-      this.state.dinnerMenu.push(this.state.menu[i])
-      break;
-    case 'drink' :
-      this.state.drinksMenu.push(this.state.menu[i])
-      break;
-    default :
-  }
-}
-console.log(this.state.appMenu)
+    for (let i = 0; i < this.state.menu.length; i++) {
+      switch (this.state.menu[i].type) {
+        case 'app':
+          this.state.appMenu.push(this.state.menu[i])
+          break
+        case 'dessert':
+          this.state.dessertMenu.push(this.state.menu[i])
+          break
+        case 'dinner':
+          this.state.dinnerMenu.push(this.state.menu[i])
+          break
+        case 'drink':
+          this.state.drinksMenu.push(this.state.menu[i])
+          break
+        default:
+      }
+    }
+    console.log(this.state.appMenu)
     return (
-      <div className="App" style={{ color: 'white'}}>
-
+      <div className="App" style={{ color: 'white' }}>
         <header>
-          <Nav/>
+          <Nav />
         </header>
         <main>
           <Switch>
-            <Route exact path="/" component={(props) => 
-              <Order {...props} 
-                menu={this.state.menu} 
-                selectedItem={this.state.selectedItem}
-              />
-            }/>
-            <Route path="/Appetizers" component={(props) => 
-              <Apps {...props} 
-                menu={this.state.appMenu} 
-                selectedItem={this.state.selectedItem} 
-              />
-            }/>
-            <Route path="/Dinner" component={(props) => 
-              <Dinner {...props} 
-                menu={this.state.dinnerMenu} 
-                selectedItem={this.state.selectedItem} 
-              />
-            }/>
-            <Route path="/Dessert" component={(props) => 
-              <Dessert {...props} 
-                menu={this.state.dessertMenu} 
-               selectedItem={this.state.selectedItem} 
-              />
-            }/>
-            <Route path="/Drinks" component={(props) => 
-              <Drinks {...props} 
-                menu={this.state.drinksMenu} 
-                selectedItem={this.state.selectedItem} 
-              />
-            }/>
+            <Route
+              exact
+              path="/"
+              component={(props) => (
+                <Order
+                  {...props}
+                  menu={this.state.menu}
+                  selectedItem={this.state.selectedItem}
+                />
+              )}
+            />
+            <Route
+              path="/Appetizers"
+              component={(props) => (
+                <Apps
+                  {...props}
+                  menu={this.state.appMenu}
+                  selectedItem={this.state.selectedItem}
+                />
+              )}
+            />
+            <Route
+              path="/Dinner"
+              component={(props) => (
+                <Dinner
+                  {...props}
+                  menu={this.state.dinnerMenu}
+                  selectedItem={this.state.selectedItem}
+                />
+              )}
+            />
+            <Route
+              path="/Dessert"
+              component={(props) => (
+                <Dessert
+                  {...props}
+                  menu={this.state.dessertMenu}
+                  selectedItem={this.state.selectedItem}
+                />
+              )}
+            />
+            <Route
+              path="/Drinks"
+              component={(props) => (
+                <Drinks
+                  {...props}
+                  menu={this.state.drinksMenu}
+                  selectedItem={this.state.selectedItem}
+                />
+              )}
+            />
           </Switch>
         </main>
       </div>
-    );
-
+    )
   }
 }
-
