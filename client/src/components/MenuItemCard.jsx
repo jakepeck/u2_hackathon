f07a3fe1e28d06759bafe27ef892aa1a11052a35
Lindsey Moss <llmodbls@gmail.com>
2021-05-27 21:11:49 -0400

    additional scaffolding of components, awaiting data

--- client/src/components/MenuItemCard.jsx
+++ client/src/components/MenuItemCard.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
+import ItemDetails from './ItemDetails'
 
 
 export default class MenuItemCard extends Component {
   render() {
     const { } = this.props
     return (
-      <div className="grid">
-
+      <div className="menu-item" onClick={() => this.props.showItem(chosenItem)}>
+        <img style={{ display: 'block' }} src={menuItem.img} alt={menuItem.name} />
+        <h3>{menuItem.name}</h3>
       </div>
     )
   }
--- client/src/components/MenuList.jsx
+++ client/src/components/MenuList.jsx
@@ -1,13 +1,23 @@
 import React, { Component } from 'react'
-
+import MenuItemCard from './MenuItemCard'
 
 export default class MenuList extends Component {
+
+  showItem = (menuItem) => {
+    this.props.history.push(`/??????/${menuItem.id}`)
+  }
+
   render() {
     const { } = this.props
-    return (
-      <div className="grid">
 
-      </div>
+    const menuList = this.props.menu.map(menuItem => {
+      return <MenuItemCard key={`${menuItem.id}${menuItem.name}`} showItem={this.showItem} menuItem={menuItem} />
+    }
+    );
+    return (
+      <div>
+        { menuList}
+      </div >
     )
   }
 }
