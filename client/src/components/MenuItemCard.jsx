c130813f6bf4d6df6685b628aff11a5ff0ab734e
Lindsey Moss <llmodbls@gmail.com>
2021-05-28 12:46:39 -0400

    corrected render errors

--- client/src/components/MenuItemCard.jsx
+++ client/src/components/MenuItemCard.jsx
@@ -4,17 +4,19 @@ import React, { Component } from 'react'
 export default class MenuItemCard extends Component {
   render() {
     const { menuItem } = this.props
+
     return (
       <div
         className="menu-item"
-        onClick={() => this.props.showItem(menuItem)}>
+        /*onClick={() => this.props.showItem(menuItem)}*/>
         <img
-          src={menuItem.img}
+          src={menuItem.image}
           alt={menuItem.name}
-          width='200'
         />
         <h4>{menuItem.name}</h4>
-        <hr />
+        <h3>Price: ${menuItem.price}</h3>
+        <div className="description">{menuItem.description}</div>
+        <button></button>
       </div>
     )
   }
