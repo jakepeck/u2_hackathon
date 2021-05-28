d9d54632b5eb33b47a627c0abae3233261445661
Jake Peck <jakepeck44@gmail.com>
2021-05-28 14:50:03 -0500

    about to initialize heroku

--- client/src/globals.js
+++ client/src/globals.js
@@ -2,6 +2,9 @@
 //   headers: { Authorization: `Bearer ${process.env.REACT_APP_TMDB_KEY}` }
 // }
 
-export const BASE_URL = 'http://localhost:3001/api'
+export const BASE_URL =
+  process.env.NODE_ENV === 'production'
+    ? `${window.location.origin}`
+    : 'http://localhost:3001/api'
 
 export const IMAGE_PATH = '#'
