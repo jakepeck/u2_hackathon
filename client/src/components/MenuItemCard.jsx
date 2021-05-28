31fa76ef07827366bddad6c5f23cb5ac45f72a43
Lindsey Moss <llmodbls@gmail.com>
2021-05-27 21:16:31 -0400

    typo corrections, improve code legibility

--- client/src/components/MenuItemCard.jsx
+++ client/src/components/MenuItemCard.jsx
@@ -6,8 +6,14 @@ export default class MenuItemCard extends Component {
   render() {
     const { } = this.props
     return (
-      <div className="menu-item" onClick={() => this.props.showItem(chosenItem)}>
-        <img style={{ display: 'block' }} src={menuItem.img} alt={menuItem.name} />
+      <div
+        className="menu-item"
+        onClick={() => this.props.showItem(chosenItem)}>
+        <img
+          style={{ display: 'block' }}
+          src={menuItem.img}
+          alt={menuItem.name}
+        />
         <h3>{menuItem.name}</h3>
       </div>
     )
--- client/src/components/MenuList.jsx
+++ client/src/components/MenuList.jsx
@@ -11,9 +11,13 @@ export default class MenuList extends Component {
     const { } = this.props
 
     const menuList = this.props.menu.map(menuItem => {
-      return <MenuItemCard key={`${menuItem.id}${menuItem.name}`} showItem={this.showItem} menuItem={menuItem} />
-    }
-    );
+      return <MenuItemCard
+        key={`${menuItem.id}${menuItem.name}`}
+        showItem={this.showItem}
+        menuItem={menuItem}
+      />
+    });
+
     return (
       <div>
         { menuList}
